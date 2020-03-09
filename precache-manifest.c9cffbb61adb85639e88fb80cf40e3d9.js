57b062d8d87a0366bc1f6be860dd858f8ba9302d
Jonathan Mack <johnny.mack1998@gmail.com>
2020-03-09 14:30:04 -0400

    Updates

---
+++ precache-manifest.c9cffbb61adb85639e88fb80cf40e3d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58e2d0683b1cb5a1a2e6e3f8e6a13d0d",
+    "url": "/poketype/index.html"
+  },
+  {
+    "revision": "8bf733d6c141a748c67d",
+    "url": "/poketype/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8bf733d6c141a748c67d",
+    "url": "/poketype/static/js/2.909ba62f.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/poketype/static/js/2.909ba62f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "618fa104425593792443",
+    "url": "/poketype/static/js/main.39ba7d2d.chunk.js"
+  },
+  {
+    "revision": "56b51c1379a8d1e9da7b",
+    "url": "/poketype/static/js/runtime-main.75ba6b1a.js"
+  }
+]);
