6336553cf73301b8683ae6537d04a64c0cb877c1
Jonathan Mack <johnny.mack1998@gmail.com>
2020-03-11 14:58:41 -0400

    leaderboard date formatted

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,14 +1,34 @@
 import React from "react";
 import Table from "react-bootstrap/Table";
 
+const MONTH_NAMES = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
 function Leaderboard(props) {
   const leaderboard = props.leaderboard;
+
+  // Convert returned date strings into Date objects
+  leaderboard.map(entry => {
+    entry["date"] = new Date(entry["date"]);
+  });
 
   return (
     <Table striped bordered hover>
       <thead>
         <tr>
-          <th>Date</th>
+          <th>Attempted On</th>
           <th>Username</th>
           <th>WPM</th>
           <th>ACC (%)</th>
@@ -17,7 +37,7 @@ function Leaderboard(props) {
       <tbody>
         {leaderboard.map((entry, i) => (
           <tr key={i}>
-            <td>{entry["date"]}</td>
+            <td>{entry["date"].toLocaleString()}</td>
             <td>{entry["username"]}</td>
             <td>{entry["wpm"]}</td>
             <td>{entry["acc"]}</td>
